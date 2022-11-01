5204f45cac736287dc85cb5eccac1782e7043cfe
Binh Duy <32291270+chestraxx@users.noreply.github.com>
2022-11-01 22:37:47 +0700

    medium 12

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Collection of Vue.js challenges
 </p>
 
 <p align='center'>
-In progress: ██████▂▁▁▁▁▁▁ 48%
+In progress: ██████▅▁▁▁▁▁▁ 51%
 </p>
 
 <p align='center'>
--- questions/12-optimize-perf-directive/App.vue
+++ questions/12-optimize-perf-directive/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { ref } from "vue"
+import { ref } from 'vue';
 
-const count = ref(0)
+const count = ref(0);
 
 setInterval(() => {
-  count.value++
-}, 1000)
+  count.value++;
+}, 1000);
 </script>
 
 <template>
-  <span>Make it never change: {{ count }}</span>
+  <span v-once>Make it never change: {{ count }}</span>
 </template>
